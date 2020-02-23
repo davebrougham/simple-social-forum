3030052b6e4b8a1a9784a6eb7659d681bc047583
davebrougham <d.brougham1@gmail.com>
2020-02-22 23:28:24 -0500

    Accounts app complete and start of groups,posts

---
+++ simplesocial/groups/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views import generic
+from groups.models import Group,GroupMember
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields = ('name','description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroup(generic.ListView):
+    model = Group
